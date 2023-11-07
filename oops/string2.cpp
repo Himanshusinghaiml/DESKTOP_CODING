7b1a8a8de6fe1b3725b56c67959f966d0ebaa6ae
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-11-07 20:50:44 +0530

    c

--- oops/string2.cpp
+++ oops/string2.cpp
@@ -4,7 +4,7 @@ int main()
 {   
     string s;
     cout<<" enter the name of the student "<<endl;
-    getline(cin,s);
+    getline(cin,s); // getline method is used to take whole string include spaces 
     cout<<" your name is :"<<s;
 
 }
